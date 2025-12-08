19fb1fac6ff809410cdce70babc009a7a282257a
Brijesh Vishwakarma <brijesh.vishwakarma@openspaceservices.com>
2025-12-08 13:09:11 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,46 +18,94 @@ const App = () => {
           <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
             <div className="grid gap-4">
               <div>
-                <img className="h-auto max-w-full rounded-base" src="/Artsify-Client-Details/img14.jpg" alt="" />
+                <img
+                  className="h-auto max-w-full rounded-base"
+                  src="/Artsify-Client-Details/img14.jpg"
+                  alt="Portrait sketch"
+                />
               </div>
               <div>
-                <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="" />
+                <img
+                  className="h-auto max-w-full rounded-base"
+                  src="/Artsify-Client-Details/img1.jpg"
+                  alt="Couple portrait"
+                />
               </div>
               <div>
-                <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="" />
-              </div>
-            </div>
-            <div className="grid gap-4">
-              <div>
-                <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
-              </div>
-              <div>
-                <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt="" />
-              </div>
-              <div>
-                <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt="" />
-              </div>
-            </div>
-            <div className="grid gap-4">
-              <div>
-                <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt="" />
-              </div>
-              <div>
-                <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt="" />
-              </div>
-              <div>
-                <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt="" />
+                <img
+                  className="h-auto max-w-full rounded-base"
+                  src="/Artsify-Client-Details/img2.jpg"
+                  alt="Sketch detail"
+                />
               </div>
             </div>
             <div className="grid gap-4">
               <div>
-                <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt="" />
+                <img
+                  className="h-auto max-w-full rounded-base"
+                  src="/Artsify-Client-Details/img3.jpg"
+                  alt="Portrait study"
+                />
               </div>
               <div>
-                <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt="" />
+                <img
+                  className="h-auto max-w-full rounded-base"
+                  src="/Artsify-Client-Details/img4.jpg"
+                  alt="Graphite artwork"
+                />
               </div>
               <div>
-                <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt="" />
+                <img
+                  className="h-auto max-w-full rounded-base"
+                  src="/Artsify-Client-Details/img5.jpg"
+                  alt="Character sketch"
+                />
+              </div>
+            </div>
+            <div className="grid gap-4">
+              <div>
+                <img
+                  className="h-auto max-w-full rounded-base"
+                  src="/Artsify-Client-Details/img6.jpg"
+                  alt="Detailed portrait"
+                />
+              </div>
+              <div>
+                <img
+                  className="h-auto max-w-full rounded-base"
+                  src="/Artsify-Client-Details/img7.jpg"
+                  alt="Fine-line sketch"
+                />
+              </div>
+              <div>
+                <img
+                  className="h-auto max-w-full rounded-base"
+                  src="/Artsify-Client-Details/img8.jpg"
+                  alt="Monochrome art"
+                />
+              </div>
+            </div>
+            <div className="grid gap-4">
+              <div>
+                <img
+                  className="h-auto max-w-full rounded-base"
+                  src="/Artsify-Client-Details/img9.jpg"
+                  alt="Creative composition"
+                />
+              </div>
+              <div>
+                <img
+                  className="h-auto max-w-full rounded-base"
+                  src="/Artsify-Client-Details/img10.jpg"
+                  alt="Creative portrait"
+                />
+              </div>
+              <div>
+                <img
+                  className="h-auto max-w-full rounded-base"
+                  src="/Artsify-Client-Details/img11.jpg"
+                  alt="Concept sketch"
+                />
               </div>
             </div>
           </div>
